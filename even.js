edaa1f353af6d60735349271fa4a272f8cc51889
Nathan Pare <nathanparebusiness@gmail.com>
2022-01-14 16:08:26 -0800

    checking even numbers with functions

---
+++ even.js
@@ -0,0 +1,16 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+//new Example
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
